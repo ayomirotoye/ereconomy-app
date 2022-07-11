3f8b48b4200936d950c2f1685e1b1c01ecba3106
Ayomide Akinrotoye <ayomirotoye@gmail.com>
2022-07-11 14:07:54 +0100

    wip:login flow

--- src/utils/apiCallEndpoints.ts
+++ src/utils/apiCallEndpoints.ts
@@ -1,11 +1,11 @@
 export const endpoints={
-    loginEndpoint:  `${process.env.PUBLIC_API_BASE_URL}/api/v1/auth/login`,
-    refreshTokenEndpoint:  `${process.env.PUBLIC_API_BASE_URL}/api/v1/auth/login`,
-    registerEndpoint: `${process.env.PUBLIC_API_BASE_URL}/api/v1/user/register`,
-    initiateCardPayment:`${process.env.PUBLIC_API_BASE_URL}/api/v1/payment/fund-account/card/initiate`,
-    verifyCardPayment:`${process.env.PUBLIC_API_BASE_URL}/api/v1/payment/card-funding/status`,
-    emailVerificationOtp:`${process.env.PUBLIC_API_BASE_URL}/api/v1/token/generate`,
-    forgotPassword:`${process.env.PUBLIC_API_BASE_URL}/api/v1/user/forgot-password`,
-    refreshToken:`${process.env.PUBLIC_API_BASE_URL}/api/v1/auth/refresh-token`,
-    userBalanceEndpoint:`${process.env.PUBLIC_API_BASE_URL}/api/v1/account/balance`
+    loginEndpoint:  `${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/auth/login`,
+    refreshTokenEndpoint:  `${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/auth/login`,
+    registerEndpoint: `${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/user/register`,
+    initiateCardPayment:`${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/payment/fund-account/card/initiate`,
+    verifyCardPayment:`${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/payment/card-funding/status`,
+    emailVerificationOtp:`${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/token/generate`,
+    forgotPassword:`${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/user/forgot-password`,
+    refreshToken:`${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/auth/refresh-token`,
+    userBalanceEndpoint:`${process.env.REACT_APP_PUBLIC_API_BASE_URL}/api/v1/account/balance`
 }
